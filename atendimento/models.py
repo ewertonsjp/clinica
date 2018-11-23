265646802ea7664619ffe6d018f1a740ed9e8a3d
Ewerton Costa <ewertonsjp@gmail.com>
2018-11-23 11:51:34 -0300

    Implementa listagem de clientes

--- atendimento/models.py
+++ atendimento/models.py
@@ -1,3 +1,7 @@
 from django.db import models
 
-# Create your models here.
+class Cliente(models.Model):    
+    nome = models.CharField(max_length=50)
+    dt_nasc = models.DateField()
+    cpf = models.CharField(max_length=11)
+    sexo = models.CharField(max_length=1)
--- atendimento/views.py
+++ atendimento/views.py
@@ -1,4 +1,11 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from . models import Cliente
 
 def home(request):
     return render(request, 'home.html', {})
+
+def cliente_list(request):
+    clientes = Cliente.objects.all()
+    return render(request, 
+        'cliente/list.html', {'clientes':clientes})
